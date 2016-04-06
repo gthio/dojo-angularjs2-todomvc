016978dcae61000c9fb4dca9343ad5e91f88ab18
gthio <gunawanthio@outlook.com>
2016-04-06 17:05:28 +0800

    Remove unused template's default files

---
+++ src/app/components/todo-app/todo-app.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core';
+import {Http} from 'angular2/http';
+
+@Component({
+  selector: 'mainApp',
+  templateUrl: 'app/components/todo-app/todo-app.html',
+  styleUrls: ['app/components/todo-app/todo-app.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ToDoApp {
+
+  constructor() {
+  }
+
+}
--- src/app/main-app.ts
+++ src/app/main-app.ts
@@ -1,9 +1,7 @@
 import {Component} from 'angular2/core';
 import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
-import {Home} from './components/home/home';
-import {About} from './components/about/about';
-import {RepoBrowser} from './components/repo-browser/repo-browser';
+import {ToDoApp} from './components/todo-app/todo-app';
 
 @Component({
   selector: 'main-app',
@@ -13,9 +11,7 @@ import {RepoBrowser} from './components/repo-browser/repo-browser';
   templateUrl: 'app/main-app.html',
 })
 @RouteConfig([
-  { path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
-  { path: '/about',      component: About,       name: 'About' },
-  { path: '/github/...', component: RepoBrowser, name: 'RepoBrowser' },
+  { path: '/home',       component: ToDoApp,     name: 'ToDoApp', useAsDefault: true },
 ])
 export class MainApp {
 
